8ced773d2c2c451a2e2a01d004cef8cb2b176d07
Eva <torresevag@hotmail.com>
2025-12-30 10:43:59 +0000

    creo que listo

--- src/models.py
+++ src/models.py
@@ -5,15 +5,50 @@ from sqlalchemy.orm import Mapped, mapped_column
 db = SQLAlchemy()
 
 class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True)
+    username= db.Column(db.String(120), unique=True, nullable=False)
+    firstname=db.Column(db.String(120))
+    lastname=db.Column(db.String(120))
+    email= db.Column(db.String(120), unique=True, nullable=False)
+    
+    def __repr__(self):
+        return f"<User {self.username}>"
 
+        
+class Post(db.Model):
+    __tablename__ = "post"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id= db.Column(db.Integer, db.ForeignKey("user.id"), nullable= False)
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+    def __repr__(self):
+        return f"<Post {self.id}>"
+
+class Media(db.Model):
+    __tablename__ = "media"
+    id = db.Column(db.Integer, primary_key=True)
+    type= db.Column(db.Enum("image", "video", name="media_type"))
+    URL= db.Column(db.String(120))
+    post_id= db.Column(db.Integer,db.ForeignKey("post.id"), nullable= False)
+
+    def __repr__(self):
+        return f"<Media {self.id}>"
+    
+class Comment(db.Model): 
+    __tablename__ = "comment"  
+    id = db.Column(db.Integer, primary_key=True)
+    comment_text= db.Column(db.String(500), nullable= False)
+    author_id= db.Column(db.Integer, db.ForeignKey("user.id"), nullable= False)
+    post_id= db.Column(db.Integer, db.ForeignKey("post.id"), nullable= False)
+
+    def __repr__(self):
+        return f"<Comment {self.id}>" 
+    
+class Follower(db.Model): 
+    __tablename__ = "follower"   
+    user_from_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable= False, primary_key=True)
+    user_to_id= db.Column(db.Integer, db.ForeignKey("user.id"), nullable= False, primary_key=True)
+    
+
+    def __repr__(self):
+        return f"<Follower {self.user_from_id} -> {self.user_to_id}>"
